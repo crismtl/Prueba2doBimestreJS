0dae23b56cd5f3a36e3293ca82f0d6a0484a20e2
Cristian Toaquiza <crissmtl@gmail.com>
2016-01-19 19:38:11 -0500

    Prueba: Funcionalidad y vistas hasta ingresar ingrediente

--- assets/app/controladores/PastelController.js
+++ assets/app/controladores/PastelController.js
@@ -1,15 +1,69 @@
-app.controller('PastelController', ['$scope', '$http', 'toastr', '$stateParams', 'PastelFactory', function($scope, $http, toastr, $stateParams, PastelFactory) {
-  toastr.info('Info', 'Se cargo la vista Pastel');
+app.controller('PastelController', ['$scope', '$http', 'toastr', '$stateParams', 'UsuarioFactory', 'PastelFactory', 'IngredienteFactory',
+  function($scope, $http, toastr, $stateParams, UsuarioFactory, PastelFactory, IngredienteFactory) {
+    toastr.info('Info', 'Se cargo la vista Pastel');
 
-  PastelFactory.busquedaPorIdUsuario({
-    idUsuario: $stateParams.idUsuario
-  }).$promise.then(
-    function success(respuesta) {
-      toastr.success('Éxito!', 'Se obtuvieron los Pasteles');
-      $scope.pasteles = respuesta;
-    },
-    function error(error) {
-      toastr.error('Error!', 'No se obtuvieron los Pasteles');
-      console.log(error);
-    });
-}]);
+    $scope.nuevoPastel = {
+      nombre: '',
+      tipo: '',
+      preparacion: ''
+    }
+
+    $scope.nuevoIngrediente = {
+      nombre: ''
+    }
+
+    PastelFactory.busquedaPorIdUsuario({
+      idUsuario: $stateParams.idUsuario
+    }).$promise.then(
+      function success(respuesta) {
+        toastr.success('Éxito!', 'Se obtuvieron los Pasteles');
+        $scope.pasteles = respuesta;
+      },
+      function error(error) {
+        toastr.error('Error!', 'No se obtuvieron los Pasteles');
+        console.log(error);
+      });
+
+    UsuarioFactory.get({
+      id: $stateParams.idUsuario
+    }).$promise.then(
+      function success(respuesta) {
+        toastr.success('Éxito!', 'Se obtuvo el Usuario');
+        $scope.usuario = respuesta;
+      },
+      function error(error) {
+        toastr.error('Error!', 'No se obtuvo el Usuario');
+        console.log(error);
+      });
+
+    $scope.agregarPastel = function() {
+      PastelFactory.save({
+        nombre: $scope.nuevoPastel.nombre,
+        tipo: $scope.nuevoPastel.tipo,
+        preparacion: $scope.nuevoPastel.preparacion,
+        idUsuario: $stateParams.idUsuario
+      }).$promise.then(
+        function success(respuesta) {
+          toastr.success('Éxito!', 'Se ingresó el Nuevo Pastel');
+        },
+        function error(error) {
+          toastr.error('Error!', 'No se ingresó el Nuevo Pastel');
+          console.log(error);
+        });
+    }
+
+    $scope.agregarIngrediente = function(pastel) {
+      IngredienteFactory.save({
+        nombre: $scope.nuevoIngrediente.nombre,
+        idPastel: pastel.id
+      }).$promise.then(
+        function success(respuesta) {
+          toastr.success('Éxito!', 'Se ingresó el Nuevo Ingrediente');
+        },
+        function error(error) {
+          toastr.error('Error!', 'No se ingresó el Nuevo Ingrediente');
+          console.log(error);
+        });
+    }
+  }
+]);
